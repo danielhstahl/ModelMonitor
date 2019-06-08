e8b5e1fc5b52df9ff15ac6c3758f89cf4c5f32f0
Daniel Stahl <danstahl1138@gmail.com>
2019-06-08 10:30:07 -0500

    working on integrated travis environment

--- build.sbt
+++ build.sbt
@@ -1,10 +1,11 @@
-// The simplest possible sbt build file is just one line:
-
+import scala.io.Source
 scalaVersion := "2.11.12"
 
 name := "modelmonitor"
 organization := "ml.dhs"
-version := "0.0.1-SNAPSHOT"
+
+val lines=Source.fromFile("VERSION").getLines.toArray
+version := lines(0)+"-SNAPSHOT"
 
 fork in Test := true
 javaOptions ++= Seq("-Xms512M", "-Xmx2048M", "-XX:MaxPermSize=2048M", "-XX:+CMSClassUnloadingEnabled")
--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
-
+import os
+thisFolder=os.path.dirname(os.path.realpath(__file__))
+with open(os.path.abspath(os.path.join(thisFolder, "..", "VERSION"))) as version_file:
+    version=version_file.read().strip()
 
 setup(
     name="modelmonitor",
-    version="0.0.1",
+    version=version,
     packages=find_packages(),
     long_description=open("README.md").read(),
     setup_requires=["pytest-runner"],
